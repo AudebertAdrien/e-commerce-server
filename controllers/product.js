4544beed0bc2fd0a651e35449383817a2921c9e1
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-03-03 10:40:07 +0100

    before new branch

--- controllers/product.js
+++ controllers/product.js
@@ -11,6 +11,7 @@ const s3bucket = new AWS.S3({
 exports.createProduct = (req, res) => {
   console.log("CreateProduct");
   const file = req.file;
+  console.log(file);
 
   let params = {
     Bucket: process.env.AWS_BUCKET_NAME,
@@ -24,6 +25,7 @@ exports.createProduct = (req, res) => {
     if (error) {
       res.status(500).json({ error: true, Message: error });
     } else {
+      console.log(data);
       const newProduct = new Product({
         ...req.body,
         imageUrl: data.Location,
@@ -37,36 +39,84 @@ exports.createProduct = (req, res) => {
   });
 };
 
+/*
+{
+  fieldname: 'file',
+  originalname: 'sharegrid-N10auyEVst8-unsplash.jpg',
+  encoding: '7bit',
+  mimetype: 'image/jpeg',
+  buffer: <Buffer ff d8 ff e0 00 10 4a 46 49 46 00 01 01 01 00 48 00 48 00 00 ff e2 02 1c 49 43 43 5f 50 52 4f 46 49 4c 45 00 01 01 00 00 02 0c 6c 63 6d 73 02 10 00 00 ... 1646064 more bytes>,
+  size: 1646114
+}
+//
+upload => S3
+{
+  ETag: '"5a1f394333fff4ba1cd1093556cd8d32"',
+  Location: 'https://my-e-commerce-bucket.s3.eu-west-1.amazonaws.com/c-d-x-5qT09yIbROk-unsplash.jpg',
+  key: 'c-d-x-5qT09yIbROk-unsplash.jpg',
+  Key: 'c-d-x-5qT09yIbROk-unsplash.jpg',
+  Bucket: 'my-e-commerce-bucket'
+}
+
+
+//getObject
+{
+  AcceptRanges: 'bytes',
+  LastModified: 2021-02-28T16:55:32.000Z,
+  ContentLength: 879172,
+  ETag: '"5a1f394333fff4ba1cd1093556cd8d32"',
+  ContentType: 'image/jpeg',
+  Metadata: {},
+  Body: <Buffer ff d8 ff e0 00 10 4a 46 49 46 00 01 01 01 00 48 00 48 00 00 ff e2 02 1c 49 43 43 5f 50 52 4f 46 49 4c 45 00 01 01 00 00 02 0c 6c 63 6d 73 02 10 00 00 ... 879122 more bytes>
+}
+*/
+
 exports.updateProduct = (req, res) => {
-  /*   console.log("UpdateProduct");
+  console.log("UpdateProduct");
   const file = req.file;
 
-  let params = {
-    Bucket: process.env.AWS_BUCKET_NAME,
-    Key: file.originalname,
-    Body: file.buffer,
-    ContentType: file.mimetype,
-    ACL: "public-read",
-  };
+  Product.findOneAndUpdate(req.params.id)
+    .then((data) => {
+      let fileToCreate = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: file.originalname,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+        ACL: "public-read",
+      };
+      let fileToRemove = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: data.s3Key,
+      };
 
+      const deleteObject = s3bucket.deleteObject(
+        fileToRemove,
+        (error, data) => {
+          if (error) {
+            res.status(500).json({ error: true, Message: error });
+          } else {
+            console.log("Product deleted!");
+          }
+        }
+      );
 
-
-  Product.findOneAndUpdate(
-    { _id: req.params.id },
-    { ...req.body, imageUrl: data.Location, s3Key: data.Key },
-    (error, data) => {
-      s3bucket.putObject(params, (error, data) => {
+      const putObject = s3bucket.putObject(fileToCreate, (error, data) => {
         if (error) {
           res.status(500).json({ error: true, Message: error });
         } else {
-          res.status(200).json(data);
+          console.log(data);
+          // res.status(200).json({ message: "Product Modifed!" });
         }
       });
-    }
-  ); */
+
+      Promise.all([deleteObject, putObject]).then((values) => {
+        console.log("insidepromise");
+      });
+    })
+    .catch((error) => res.status(400).json({ error }));
 };
 
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = async (req, res) => {
   console.log("DeleteProduct");
   Product.findOneAndDelete({ _id: req.params.id })
     .then((data) => {
