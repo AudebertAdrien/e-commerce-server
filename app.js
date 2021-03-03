bf455ed7c822694f6a1840d46f4613f30d38e84b
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-03-03 18:28:13 +0100

    fix

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose
   .catch((error) => console.log("Failed to connect to MongoDB :", error));
 
 const corsOptions = {
-  origin: "https://my-e-commerce-client.herokuapp.com/",
+  origin: "https://my-e-commerce-client.herokuapp.com",
   optionsSuccessStatus: 200,
 };
 
