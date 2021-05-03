e1ec2697ee54ba5ed8e4776415812221c21e8673
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-05-03 18:39:46 +0200

    data gouv new project

--- app.js
+++ app.js
@@ -1,43 +1,21 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 const cors = require("cors");
-// const { checkUser, requireAuth } = require("./middlewares/auth");
-
-const productRoute = require("./routes/product");
-const userRoute = require("./routes/user");
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER_PASS}"@cluster0.saoyk.mongodb.net/<dbname>?retryWrites=true&w=majority`,
+    `mongodb+srv://adrien:${process.env.DB_USER_PASS}@cluster0.9sobl.mongodb.net/data-gouv?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
   )
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.log("Failed to connect to MongoDB :", error));
 
 const corsOptions = {
-  origin: [
-    "https://my-e-commerce-client.herokuapp.com",
-    "http://localhost:8080",
-  ],
+  origin: "*",
   optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
-
-app.use(bodyParser.json());
-app.use(cookieParser());
-
-/*
-app.use("*", checkUser);
-app.get("/jwtid", requireAuth, (req, res) => {
-  res.status(200).send(res.locals.user.id);
-});
-*/
-
-app.use("/api/user", userRoute);
-app.use("/api/products", productRoute);
 
 module.exports = app;
