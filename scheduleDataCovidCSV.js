b31ad817eda13bc23ffe94f157567d341c9afd99
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-05-26 11:26:47 +0200

    remove pass

--- scheduleDataCovidCSV.js
+++ scheduleDataCovidCSV.js
@@ -40,7 +40,7 @@ module.exports = function () {
         formatTheNewCSVFile()
           .then(function () {
             exec(
-              "mongoimport --host=cluster0-shard-00-01.cxrmv.mongodb.net:27017 --db data-gouv --collection incidence --type csv --drop --headerline  --file ./docs/output.csv --authenticationDatabase admin --ssl --username adrien --password azerty",
+              `mongoimport --host=cluster0-shard-00-01.cxrmv.mongodb.net:27017 --db data-gouv --collection incidence --type csv --drop --headerline  --file ./docs/output.csv --authenticationDatabase admin --ssl --username adrien --password ${process.env.DB_USER_PASS}`,
               (error, stdout, stderr) => {
                 if (error) {
                   console.log(`error: ${error.message}`);
