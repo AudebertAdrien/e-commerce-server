0513fd6ff939f015831038b06f94fb4bc64c6818
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-05-28 17:18:55 +0200

    test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { exec } = require("child_process");
-console.log("hahaha!!!");
+console.log("zozo!!!");
 
 exec(
   `mongoimport --host=cluster0-shard-00-01.cxrmv.mongodb.net:27017 --db data-gouv --collection incidence --type csv --drop --headerline  --file ./docs/output.csv --authenticationDatabase admin --ssl --username adrien --password ${process.env.DB_USER_PASS}`,
