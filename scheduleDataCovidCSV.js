b59c570ff7ccc88f5d4437183fc372ca3943c30d
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-05-28 13:00:30 +0200

    test in production

--- scheduleDataCovidCSV.js
+++ scheduleDataCovidCSV.js
@@ -6,61 +6,59 @@ const { exec } = require("child_process");
 
 const formatTheNewCSVFile = require("./formatTheNewCSVFile");
 
-module.exports = function () {
-  console.log("scheduleDataCovidCSV");
-  let time = 0;
-  cron.schedule(
-    "*/2 * * * *",
-    () => {
-      console.log(time++);
-      async function downloadCsv() {
-        const url =
-          "https://www.data.gouv.fr/fr/datasets/r/19a91d64-3cd3-42fc-9943-d635491a4d76";
+console.log("scheduleDataCovidCSV");
+cron.schedule(
+  "*/3 * * * *",
+  () => {
+    console.log("inside");
+    async function downloadCsv() {
+      const url =
+        "https://www.data.gouv.fr/fr/datasets/r/19a91d64-3cd3-42fc-9943-d635491a4d76";
 
-        const csvPath = path.resolve(__dirname, "docs", "output.csv");
+      const csvPath = path.resolve(__dirname, "docs", "output.csv");
 
-        const writer = fs.createWriteStream(csvPath);
+      const writer = fs.createWriteStream(csvPath);
 
-        const response = await axios({
-          url: url,
-          method: "GET",
-          responseType: "stream",
-        });
-
-        response.data.pipe(writer);
-
-        return new Promise((resolve, reject) => {
-          writer.on("finish", resolve);
-          writer.on("error", reject);
-        });
-      }
-      downloadCsv().then(() => {
-        console.log("1");
-        // remove the character ";" by an empty character " "
-        formatTheNewCSVFile()
-          .then(function () {
-            exec(
-              `mongoimport --host=cluster0-shard-00-01.cxrmv.mongodb.net:27017 --db data-gouv --collection incidence --type csv --drop --headerline  --file ./docs/output.csv --authenticationDatabase admin --ssl --username adrien --password ${process.env.DB_USER_PASS}`,
-              (error, stdout, stderr) => {
-                if (error) {
-                  console.log(`error: ${error.message}`);
-                  return;
-                }
-                if (stderr) {
-                  console.log(`stderr:, ${stderr}`);
-                  return;
-                }
-                console.log(`stdout: ${stdout}`);
-              }
-            );
-          })
-          .catch(function (err) {
-            console.log("error here: " + err);
-          });
+      const response = await axios({
+        url: url,
+        method: "GET",
+        responseType: "stream",
       });
-    },
-    {
-      scheduled: true,
+
+      response.data.pipe(writer);
+
+      return new Promise((resolve, reject) => {
+        writer.on("finish", resolve);
+        writer.on("error", reject);
+      });
     }
-  );
-};
+    downloadCsv().then(() => {
+      console.log("downloadCsv then");
+      // remove the character ";" by an empty character " "
+      formatTheNewCSVFile()
+        .then(function () {
+          console.log("mongoimport");
+          exec(
+            `mongoimport --host=cluster0-shard-00-01.cxrmv.mongodb.net:27017 --db data-gouv --collection incidence --type csv --drop --headerline  --file ./docs/output.csv --authenticationDatabase admin --ssl --username adrien --password ${process.env.DB_USER_PASS}`,
+            (error, stdout, stderr) => {
+              if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+              }
+              if (stderr) {
+                console.log(`stderr:, ${stderr}`);
+                return;
+              }
+              console.log(`stdout: ${stdout}`);
+            }
+          );
+        })
+        .catch(function (err) {
+          console.log("error here: " + err);
+        });
+    });
+  },
+  {
+    scheduled: true,
+  }
+);
