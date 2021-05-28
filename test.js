523b0cb746cf661bbdaf976b9ce82fb829072e0c
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-05-28 17:28:18 +0200

    env variable

--- test.js
+++ test.js
@@ -1,17 +1,14 @@
 const { exec } = require("child_process");
 console.log("zozo!!!");
 
-exec(
-  `mongoimport --host=cluster0-shard-00-01.cxrmv.mongodb.net:27017 --db data-gouv --collection incidence --type csv --drop --headerline  --file ./docs/output.csv --authenticationDatabase admin --ssl --username adrien --password ${process.env.DB_USER_PASS}`,
-  (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr:, ${stderr}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
+exec(process.env.MONGODB_URI, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
   }
-);
+  if (stderr) {
+    console.log(`stderr:, ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
