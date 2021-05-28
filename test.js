f4c0689492994cb2079579998a8e112fce5ed6a8
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-05-28 16:57:47 +0200

    test exec

--- test.js
+++ test.js
@@ -1,12 +1,17 @@
 const { exec } = require("child_process");
-exec("ls -la", (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
+console.log("coco!!!");
+
+exec(
+  `mongoimport --host=cluster0-shard-00-01.cxrmv.mongodb.net:27017 --db data-gouv --collection incidence --type csv --drop --headerline  --file ./docs/output.csv --authenticationDatabase admin --ssl --username adrien --password ${process.env.DB_USER_PASS}`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr:, ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
   }
-  if (stderr) {
-    console.log(`stderr:, ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
+);
