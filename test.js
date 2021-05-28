d04f615e8cab49fd3b6564aff4d87110ac689588
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-05-28 16:47:43 +0200

    test exec in prod

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
-const cron = require("node-cron");
-
-let i = 0;
-cron.schedule("*/5 * * * * *", () => {
-  console.log(i++);
+const { exec } = require("child_process");
+exec("ls -la", (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr:, ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
 });
