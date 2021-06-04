8b0d217a7ba5e0ec97f2836444f80b0962dbdd22
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-06-04 12:14:20 +0200

    test in prod

--- scheduleDataCovidCSV.js
+++ scheduleDataCovidCSV.js
@@ -36,8 +36,8 @@ module.exports = function () {
       downloadCsv().then(() => {
         console.log("downloadCsv then");
         // remove the character ";" by an empty character " "
-        formatTheNewCSVFile();
-        /* .then(function () {
+        formatTheNewCSVFile()
+          .then(function () {
             console.log("mongoimport");
             exec(
               `mongoimport --host=cluster0-shard-00-01.cxrmv.mongodb.net:27017 --db data-gouv --collection incidence --type csv --drop --headerline  --file ./docs/output.csv --authenticationDatabase admin --ssl --username adrien --password ${process.env.DB_USER_PASS}`,
@@ -53,10 +53,11 @@ module.exports = function () {
                 console.log(`stdout: ${stdout}`);
               }
             );
+            console.log("after mongoimport");
           })
           .catch(function (err) {
             console.log("error here: " + err);
-          }); */
+          });
       });
     },
     {
