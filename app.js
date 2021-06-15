370496a3f57ee4c9f676dd4248f42f106174d3b7
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-06-15 21:15:32 +0200

    connection to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
 const corsOptions = {
   origin: [
@@ -15,20 +15,22 @@ const corsOptions = {
 app.use(express.text());
 app.use(cors(corsOptions));
 
-/* const DATABASE = "data-gouv";
-  const uri = `mongodb+srv://azerty:${process.env.DB_USER_PASS}@cluster1.lv4ww.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
+const DATABASE = "items";
+const uri = `mongodb+srv://adrien:${process.env.DB_USER_PASS}@cluster1.lv4ww.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
 
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  client.connect((err) => {
-    if (err) return console.error(err);
-    console.log("Connected successfully to server");
-    const db = client.db("items");
-    // perform actions on the collection object
-    client.close();
-  }); */
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client.connect((err) => {
+  if (err) return console.error(err);
+  console.log("Connected successfully to server");
+  // const db = client.db("items");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.get("/", (req, res) => {
   console.log("test!!!!!!!!!!!!!!!!");
   res.status(200);
