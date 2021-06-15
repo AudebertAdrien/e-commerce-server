7bd1f14d59a30e66a0a9b439d307c9a33f5f969f
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-06-15 20:48:05 +0200

    new start

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
-
-const newArrayOfDepartmentsAndIncidences = require("./newArrayOfDepartmentsAndIncidences");
-const scheduleDataCovidCSV = require("./scheduleDataCovidCSV");
+// const MongoClient = require("mongodb").MongoClient;
 
 const corsOptions = {
   origin: [
@@ -18,36 +15,23 @@ const corsOptions = {
 app.use(express.text());
 app.use(cors(corsOptions));
 
-// get every 24 hours an updated csv file of covid data 19 from data.gouv.fr
+/* const DATABASE = "data-gouv";
+  const uri = `mongodb+srv://azerty:${process.env.DB_USER_PASS}@cluster1.lv4ww.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
 
-const uri = `mongodb+srv://adrien:${process.env.DB_USER_PASS}@cluster0.cxrmv.mongodb.net/data-gouv?retryWrites=true&w=majority`;
-const DATABASE = "data-gouv";
-
-MongoClient.connect(
-  uri,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    scheduleDataCovidCSV();
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  client.connect((err) => {
     if (err) return console.error(err);
     console.log("Connected successfully to server");
-    const db = client.db(DATABASE);
-
-    app.post("/", function (req, res) {
-      try {
-        db.collection("incidence")
-          .find({ jour: { $eq: req.body } })
-          .sort({ dep: 1, _id: 1 })
-          .toArray()
-          .then((dataCovid19) => {
-            let departmentsAndIncidence =
-              newArrayOfDepartmentsAndIncidences(dataCovid19);
-            res.status(200).json(departmentsAndIncidence);
-          });
-      } catch (error) {
-        console.error(error);
-      }
-    });
-  }
-);
+    const db = client.db("items");
+    // perform actions on the collection object
+    client.close();
+  }); */
+app.get("/", (req, res) => {
+  console.log("test!!!!!!!!!!!!!!!!");
+  res.status(200);
+});
 
 module.exports = app;
